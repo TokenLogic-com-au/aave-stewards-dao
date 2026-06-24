de360f16943bcab747239de11d6c3d36c906809b
Juan Ignacio Ubeira <juani@tokenlogic.xyz>
2026-06-24 15:45:06 -0300

    Adjust owner / guardian.

--- scripts/DeployCctpBridgeSteward.s.sol
+++ scripts/DeployCctpBridgeSteward.s.sol
@@ -7,6 +7,10 @@ import {AaveV3Arbitrum} from "aave-address-book/AaveV3Arbitrum.sol";
 import {AaveV3Optimism} from "aave-address-book/AaveV3Optimism.sol";
 import {AaveV3Polygon} from "aave-address-book/AaveV3Polygon.sol";
 import {AaveV3Base} from "aave-address-book/AaveV3Base.sol";
+import {GovernanceV3Arbitrum} from "aave-address-book/GovernanceV3Arbitrum.sol";
+import {GovernanceV3Optimism} from "aave-address-book/GovernanceV3Optimism.sol";
+import {GovernanceV3Polygon} from "aave-address-book/GovernanceV3Polygon.sol";
+import {GovernanceV3Base} from "aave-address-book/GovernanceV3Base.sol";
 import {
   ArbitrumScript,
   BaseScript,
@@ -18,7 +22,6 @@ import {CctpBridgeSteward} from "src/bridges/cctp/CctpBridgeSteward.sol";
 import {CctpConstants} from "src/bridges/cctp/CctpConstants.sol";
 
 address constant TOKEN_LOGIC = 0x3765A685a401622C060E5D700D9ad89413363a91;
-address constant GUARDIAN = 0x3765A685a401622C060E5D700D9ad89413363a91;
 bytes32 constant SALT = "Aave CCTP Bridge";
 
 contract DeployCctpBridgeArbitrum is ArbitrumScript {
@@ -26,8 +29,8 @@ contract DeployCctpBridgeArbitrum is ArbitrumScript {
     new CctpBridgeSteward{salt: SALT}(
       CctpConstants.ARBITRUM_TOKEN_MESSENGER,
       CctpConstants.ARBITRUM_USDC,
-      TOKEN_LOGIC,
-      GUARDIAN,
+      GovernanceV3Arbitrum.EXECUTOR_LVL_1, // Owner
+      TOKEN_LOGIC, // Guardian
       address(AaveV3Arbitrum.COLLECTOR)
     );
   }
@@ -38,8 +41,8 @@ contract DeployCctpBridgeOptimism is OptimismScript {
     new CctpBridgeSteward{salt: SALT}(
       CctpConstants.OPTIMISM_TOKEN_MESSENGER,
       CctpConstants.OPTIMISM_USDC,
-      TOKEN_LOGIC,
-      GUARDIAN,
+      GovernanceV3Optimism.EXECUTOR_LVL_1, // Owner
+      TOKEN_LOGIC, // Guardian
       address(AaveV3Optimism.COLLECTOR)
     );
   }
@@ -50,8 +53,8 @@ contract DeployCctpBridgePolygon is PolygonScript {
     new CctpBridgeSteward{salt: SALT}(
       CctpConstants.POLYGON_TOKEN_MESSENGER,
       CctpConstants.POLYGON_USDC,
-      TOKEN_LOGIC,
-      GUARDIAN,
+      GovernanceV3Polygon.EXECUTOR_LVL_1, // Owner
+      TOKEN_LOGIC, // Guardian
       address(AaveV3Polygon.COLLECTOR)
     );
   }
@@ -60,7 +63,11 @@ contract DeployCctpBridgePolygon is PolygonScript {
 contract DeployCctpBridgeBase is BaseScript {
   function run() external broadcast {
     new CctpBridgeSteward{salt: SALT}(
-      CctpConstants.BASE_TOKEN_MESSENGER, CctpConstants.BASE_USDC, TOKEN_LOGIC, GUARDIAN, address(AaveV3Base.COLLECTOR)
+      CctpConstants.BASE_TOKEN_MESSENGER,
+      CctpConstants.BASE_USDC,
+      GovernanceV3Base.EXECUTOR_LVL_1, // Owner
+      TOKEN_LOGIC, // Guardian
+      address(AaveV3Base.COLLECTOR)
     );
   }
 }
